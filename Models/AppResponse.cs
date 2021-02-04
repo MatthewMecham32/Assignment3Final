9a2fa6ab1f81f3a5ab791b16b2a61ceba7bb2fc0
Matthew Mecham <matthewmecham32@gmail.com>
2021-02-03 17:58:35 -0700

    Add project files.

---
+++ Models/AppResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3Final.Models
+{
+    public class AppResponse
+    {
+        [Required(ErrorMessage ="Category is a required field")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Title is a required field")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Year is a required field")]
+        public int Year { get; set; }
+
+        [Required(ErrorMessage = "Director is a required field")]
+        public string Director { get; set; }
+
+        [Required(ErrorMessage = "Rating is a required field")]
+        public string Rating { get; set; }
+
+        public bool? Edited { get; set; }
+        public string LentTo { get; set; }
+        [StringLength(20)]
+        public string Notes { get; set; }
+
+    }
+}
---
+++ Models/TempStorage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment3Final.Models
+{
+    public static class TempStorage
+    {
+        private static List<AppResponse> entries = new List<AppResponse>();
+
+        public static IEnumerable<AppResponse> Entry => entries;
+
+        public static void AddEntry(AppResponse entry)
+        {
+            entries.Add(entry);
+        }
+    }
+}
